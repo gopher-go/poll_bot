3c41c317a9844e78f7a03a84b23fa76d4fa3f2cc
andrew <andrewkovalenok@gmail.com>
2020-08-01 23:26:15 +0300

    add stats caching

--- stats.go
+++ stats.go
@@ -6,31 +6,43 @@ import (
 	"net/http"
 
 	"github.com/andrewkav/viber"
+	"github.com/coocood/freecache"
 )
 
 type statsResponse struct {
 	Total int `json:"total"`
 }
 
+var (
+	statsCacheKey = []byte("stats")
+	statsCache    = freecache.NewCache(512 * 1024 * 1024)
+)
+
 func handleStats(_ poll, _ *viber.Viber, s *storage, w http.ResponseWriter, _ *http.Request) {
-	count, err := s.PersistCount()
-	if err != nil {
-		log.Printf("Error reading count: %v", err)
-		http.Error(w, "Error reading count", http.StatusBadRequest)
-		return
-	}
+	var statsRespBytes []byte
 
-	resp := statsResponse{
-		Total: count,
-	}
-
-	json, err := json.Marshal(&resp)
+	var err error
+	statsRespBytes, err = statsCache.Get(statsCacheKey)
 	if err != nil {
-		log.Printf("Error marshalling json: %v", err)
-		http.Error(w, "Error marshalling json", http.StatusBadRequest)
-		return
+		count, err := s.PersistCount()
+		if err != nil {
+			log.Printf("Error reading count: %v", err)
+			http.Error(w, "Error reading count", http.StatusBadRequest)
+			return
+		}
+
+		statsRespBytes, err = json.Marshal(statsResponse{
+			Total: count,
+		})
+		if err != nil {
+			log.Printf("Error marshalling json: %v", err)
+			http.Error(w, "Error marshalling json", http.StatusBadRequest)
+			return
+		}
+
+		_ = statsCache.Set(statsCacheKey, statsRespBytes, 5)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
+	w.Write(statsRespBytes)
 }
